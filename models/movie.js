95d6434a73a60092d9757f30f65bd8d36ee48628
Vijay <vijaymudaliyar224@gmail.com>
2025-03-30 22:32:32 +0530

    again 301d3:14:00 src

--- models/movie.js
+++ models/movie.js
@@ -1,22 +1,56 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    releaseDate: Date,
-    genre: String,
-    duration: Number, // Duration in minutes
-    image: String, // URL of the movie poster
-    director: String,
-    cast: [String], // Array of cast members
-    proposedChanges: {
-        type: Object,
-        default: null
+    title: {
+        type: String,
+        required: true,
+        trim: true
     },
-    approved: {
-        type: Boolean,
-        default: true
+    description: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    releaseDate: {
+        type: Date,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    director: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    language: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    cast: [{
+        type: String,
+        trim: true
+    }],
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    updatedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Movie', movieSchema);
